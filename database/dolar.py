17244403d06b9c5482951232ef8f71d16b6e0477
biston.nunes <biston.nunes@gmail.com>
2020-09-23 13:58:59 -0300

    update dolar

--- database/dolar.py
+++ database/dolar.py
@@ -7,9 +7,11 @@ with open('../files/dolar_brl_historio.csv', 'r') as arquivo_csv:
     for idx, colunm in enumerate(reader):
         if idx > 1:
             collection = 'historical_data'
-            keys = ['date', 'dolar']
-            values = [utils.string_to_date(colunm[0]), utils.string_to_float(colunm[1])]
+            keys = ['date', 'dolar', 'open_dolar', 'max_dolar', 'min_dolar', 'var_dolar']
+            values = [utils.string_to_date(colunm[0]), utils.string_to_float(colunm[1]), utils.string_to_float(colunm[2]), utils.string_to_float(colunm[3]), utils.string_to_float(colunm[4]), utils.string_to_float(colunm[5])]
             if server.db_select(collection, [keys[0]], [values[0]]) == None:
                 server.db_insert(collection, keys, values)
             else:
-                server.db_update(collection, {keys[0]: values[0]}, {keys[1]: values[1]})
+                for i, *_ in enumerate(colunm):
+                    if i > 1:
+                        server.db_update(collection, {keys[0]: values[0]}, {keys[i]: values[i]})
