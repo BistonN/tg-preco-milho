0fdab6554c12e851b037ec452a9fa6a58a6a5938
biston.nunes <biston.nunes@gmail.com>
2020-10-12 21:26:18 -0300

    doing some tests

--- database/corn_br.py
+++ database/corn_br.py
@@ -1,38 +1,39 @@
 import csv
 import server as server
 import utils
+from datetime import timedelta
 
-print('\n\nImportando dados Semanais do Preço do Milho\n\n')
-with open('../files/milho_br_semanal.csv', 'r') as arquivo_csv:
-    reader = csv.reader(arquivo_csv, delimiter = ',')
-    for idx, colunm in enumerate(reader):
-        if idx > 1:
-            date = colunm[0].replace(',','')
-            date = date.split(' ')
-            date = utils.format_number(date[1], 2) + '.' + utils.format_number(str(utils.mounth_numeric.get(date[0])), 2) + '.' + date[2]
+# print('\n\nImportando dados Semanais do Preço do Milho\n\n')
+# with open('../files/milho_br_semanal.csv', 'r') as arquivo_csv:
+#     reader = csv.reader(arquivo_csv, delimiter = ',')
+#     for idx, colunm in enumerate(reader):
+#         if idx > 1:
+#             date = colunm[0].replace(',','')
+#             date = date.split(' ')
+#             date = utils.format_number(date[1], 2) + '.' + utils.format_number(str(utils.mounth_numeric.get(date[0])), 2) + '.' + date[2]
 
-            collection = 'historical_data_weekly'
-            keys = ['date', 'corn_br', 'open_corn_br', 'max_corn_br', 'min_corn_br', 'vol_corn_br', 'var_corn_br']
-            values = [utils.string_to_date(date), utils.string_to_float(colunm[1]), utils.string_to_float(colunm[2]), utils.string_to_float(colunm[3]), utils.string_to_float(colunm[4]), utils.string_to_float(colunm[5]), utils.string_to_float(colunm[6])]
+#             collection = 'historical_data_weekly'
+#             keys = ['date', 'corn_br', 'open_corn_br', 'max_corn_br', 'min_corn_br', 'vol_corn_br', 'var_corn_br']
+#             values = [utils.string_to_date(date), utils.string_to_float(colunm[1]), utils.string_to_float(colunm[2]), utils.string_to_float(colunm[3]), utils.string_to_float(colunm[4]), utils.string_to_float(colunm[5]), utils.string_to_float(colunm[6])]
 
 
-            if server.db_select(collection, [keys[0]], [utils.string_to_date(date)]) == None:
-                server.db_insert(collection, keys, values)
-            else:
-                for i, *_ in enumerate(colunm):
-                    if i > 1:
-                        server.db_update(collection, {keys[0]: values[0]}, {keys[i]: values[i]})
+#             if server.db_select(collection, [keys[0]], [utils.string_to_date(date)]) == None:
+#                 server.db_insert(collection, keys, values)
+#             else:
+#                 for i, *_ in enumerate(colunm):
+#                     if i > 1:
+#                         server.db_update(collection, {keys[0]: values[0]}, {keys[i]: values[i]})
 
 print('\n\nImportando dados Diarios do Preço do Milho\n\n')
-with open('../files/milho_br.csv', 'r') as arquivo_csv:
+with open('../files/milho2020.csv', 'r') as arquivo_csv:
     reader = csv.reader(arquivo_csv, delimiter = ',')
     for idx, colunm in enumerate(reader):
-        if idx > 1:
+        if idx >= 1:
             collection = 'historical_data_daily'
             keys = ['date', 'corn_br', 'open_corn_br', 'max_corn_br', 'min_corn_br', 'vol_corn_br', 'var_corn_br']
             values = [colunm[0], utils.string_to_float(colunm[1]), utils.string_to_float(colunm[2]), utils.string_to_float(colunm[3]), utils.string_to_float(colunm[4]), utils.string_to_float(colunm[5]), utils.string_to_float(colunm[6])]
 
-            values[0] = utils.string_to_date(values[0].replace('/', '.'))
+            values[0] = utils.string_to_date(values[0].replace('/', '.')) 
 
             if server.db_select(collection, [keys[0]], [values[0]]) == None:
                 server.db_insert(collection, keys, values)
--- database/generate_dataset_2.py
+++ database/generate_dataset_2.py
@@ -14,47 +14,42 @@ def get_last_not_null_value(key, corn_1_before, corn_2_before, corn_3_before):
             results = corn_3_before.get(key)
             if corn_3_before.get(key) == 0 or corn_3_before.get(key) == '' or corn_3_before.get(key) == None:
                 results = 0
-        
         return results
-
-    results = corn_1_before.get(key)
-    return results
+    return corn_1_before.get(key)
 
 
-writer = csv.writer(open("../files/dataset_3.csv", 'w'))
+writer = csv.writer(open("../files/dataset_biston.csv", 'w'))
 
-writer.writerow(['date', 'corn_br', 'corn_usd', 'dolar', 'corn_br-1', 'corn_br-2', 'corn_br-3', 'max_corn_br-1', 'min_corn_br-1', 'open_corn_br-1', 'var_corn_br-1', 'vol_corn_br-1']) 
+writer.writerow(['date', 'corn_br', 'corn_br-1', 'corn_br-2', 'corn_br-3', 'max_corn_br-1', 'min_corn_br-1', 'open_corn_br-1', 'var_corn_br-1', 'vol_corn_br-1']) 
 
-year = 2013
+year = 2018
 month = 1
 
 corn_br_days_before = [{}, {}, {}]
 print(corn_br_days_before )
 
-while year <= 2019 and month <= 12:
+while year <= 2021 and month <= 12:
     for *week, _, _ in calendar.monthcalendar(year, month):
         for day in week:
             if day != 0:
                 results = server.db_select('historical_data_daily', ['date'], [dt.datetime(year, month, day)])
-                
-                if results.get('corn_br'):
-                    copy_results = results.copy()
-                    corn_br_days_before.append(copy_results)
+                if results:
+                    if results.get('corn_br'): 
+                        copy_results = results.copy()
+                        corn_br_days_before.append(copy_results)
 
-                    corn_3_before = corn_br_days_before.pop(0)
-                    corn_2_before = corn_br_days_before[-2]
-                    corn_1_before = corn_br_days_before[-3]
+                        corn_3_before = corn_br_days_before.pop(0)
+                        corn_2_before = corn_br_days_before[-3]
+                        corn_1_before = corn_br_days_before[-2]
 
+                        max_corn_br_1_before = get_last_not_null_value('max_corn_br', corn_1_before, corn_2_before, corn_3_before)
+                        min_corn_br_1_before = get_last_not_null_value('min_corn_br', corn_1_before, corn_2_before, corn_3_before)
+                        open_corn_br_1_before = get_last_not_null_value('open_corn_br', corn_1_before, corn_2_before, corn_3_before)
+                        var_corn_br_1_before = get_last_not_null_value('var_corn_br', corn_1_before, corn_2_before, corn_3_before)
+                        vol_corn_br_1_before = get_last_not_null_value('vol_corn_br', corn_1_before, corn_2_before, corn_3_before)
 
-
-                    max_corn_br_1_before = get_last_not_null_value('max_corn_br', corn_1_before, corn_2_before, corn_3_before)
-                    min_corn_br_1_before = get_last_not_null_value('min_corn_br', corn_1_before, corn_2_before, corn_3_before)
-                    open_corn_br_1_before = get_last_not_null_value('open_corn_br', corn_1_before, corn_2_before, corn_3_before)
-                    var_corn_br_1_before = get_last_not_null_value('var_corn_br', corn_1_before, corn_2_before, corn_3_before)
-                    vol_corn_br_1_before = get_last_not_null_value('vol_corn_br', corn_1_before, corn_2_before, corn_3_before)
-
-                    writer.writerow([results.get('date'), results.get('corn_br'), results.get('corn_usd'), results.get('dolar'), corn_1_before.get('corn_br'), corn_2_before.get('corn_br'), corn_3_before.get('corn_br'), max_corn_br_1_before, min_corn_br_1_before, open_corn_br_1_before, var_corn_br_1_before, vol_corn_br_1_before]) 
-                    print(results.get('date'))
+                        writer.writerow([results.get('date'), results.get('corn_br'), corn_1_before.get('corn_br'), corn_2_before.get('corn_br'), corn_3_before.get('corn_br'), max_corn_br_1_before, min_corn_br_1_before, open_corn_br_1_before, var_corn_br_1_before, vol_corn_br_1_before]) 
+                        print(results.get('date'))
     if month + 1 == 13:
         month = 1
         year = year + 1
